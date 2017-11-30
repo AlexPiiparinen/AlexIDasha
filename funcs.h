491ef3f6f689505a4539f44e330888e76a95c3b9
AlexPiiparinen <piiparinen@yandex.ru>
2017-11-30 18:14:38 +0300

    Add fun + and - part 2

---
+++ funcs.h
@@ -0,0 +1,8 @@
+ // код файла funcs.h
+#ifndef _FUNCS_H_ // если им€ _FUNCS_H_ ещЄ не определено
+#define _FUNCS_H_ // определить им€ _FUNCS_H_
+
+double MySum(double, double); 
+double MySub(double, double); 
+
+#endif // если им€ _FUNCS_H_ уже определено, повторно не определ€ть
